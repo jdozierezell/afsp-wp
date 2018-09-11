06ccbf366284f948349c1add49fee19bfd3c1c79
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2018-09-11 09:44:22 -0400

    super simplified videos grid

--- wp-content/themes/afsp/page-templates/grid-videos.php
+++ wp-content/themes/afsp/page-templates/grid-videos.php
@@ -6,24 +6,7 @@
  */
 
 get_header(); 
-get_template_part('template-parts/title');
-if(have_posts()) : while(have_posts()) : the_post(); ?>		
-        <section class="container" style="text-align: center; font-weight: bold;"><?php the_content(); ?></section>
-        <section class="grid">
-            <?php if ( have_rows( 'vg_videos' ) ) : while ( have_rows( 'vg_videos' ) ) : the_row; ?>
-                <div class="videoEmbed">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/H44tfaLvp8I?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                </div>
-            <?php
-                endwhile;
-            endif;
-            ?>
-        </section>   
-	
-<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/isotope.pkgd.min.js"></script>
+get_template_part( 'template-parts/title' );
 
-<?php
-    endwhile;
-endif;
 get_footer();
 ?>
