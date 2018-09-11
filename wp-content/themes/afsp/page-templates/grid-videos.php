b9ec2e458bc6a8c6fb43ca2801bcf7032beae7b7
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2018-09-11 09:45:42 -0400

    slightly more complicated videos grid

--- wp-content/themes/afsp/page-templates/grid-videos.php
+++ wp-content/themes/afsp/page-templates/grid-videos.php
@@ -7,6 +7,12 @@
 
 get_header(); 
 get_template_part( 'template-parts/title' );
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>		
+	
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/isotope.pkgd.min.js"></script>
 
+<?php
+    endwhile;
+endif;
 get_footer();
 ?>
