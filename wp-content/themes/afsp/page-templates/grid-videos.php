a466ed350eac0828bb44304ac4e0978f84c0cea1
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2018-09-11 09:59:55 -0400

    updated scss videos grid

--- wp-content/themes/afsp/page-templates/grid-videos.php
+++ wp-content/themes/afsp/page-templates/grid-videos.php
@@ -11,8 +11,8 @@ if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <section class="container" style="text-align: center; font-weight: bold;"><?php the_content(); ?></section>
         <section class="grid">
             <?php if ( have_rows( 'vg_videos' ) ) : while ( have_rows( 'vg_videos' ) ) : the_row(); ?>
-                <div class="videoEmbed">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/H44tfaLvp8I?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <div class="grid__item--video">
+                    <iframe src="https://www.youtube.com/embed/H44tfaLvp8I?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 </div>
             <?php
                 endwhile;
