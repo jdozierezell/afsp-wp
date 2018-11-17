c69c6ddcc46e3d470d744b06a98c288d1689f988
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2018-11-16 20:58:50 -0500

    removing survivor day registration

--- wp-content/themes/afsp/single-survivor_day.php
+++ wp-content/themes/afsp/single-survivor_day.php
@@ -112,7 +112,7 @@ if ( have_posts() ) :
 				endif;
 				// if ( get_field( 'sd_registration_link' ) ) :
 				?>
-					<a class="features__button" href="https://www.surveymonkey.com/r/99JL2FB" target="_blank">Register today</a>
+					<!-- <a class="features__button" href="https://www.surveymonkey.com/r/99JL2FB" target="_blank">Register today</a> -->
 				<?php // endif; ?>
 				<p><i>In the interest of creating a safe space for loss survivors, some events are exclusively for survivors of suicide loss. In addition, many events cannot accommodate children and teens.  If you have any questions, please contact the event host directly.</i></p>
 			</div>
@@ -120,6 +120,7 @@ if ( have_posts() ) :
 				<?php if ( '' !== get_field( 'sd_custom_date' ) ) : ?>
 					<p><em>Note: This Survivor Day event will be held on <strong><?php echo esc_html( get_field( 'sd_custom_date' ) ); ?></strong> instead of Nov. 17, 2018.</em></p>
 				<?php endif; ?>
+				<p><strong>Online Registration is now closed. If you would like to attend an event please contact the main organizer by email. In addition, a Survivor Day Live program will be streamed on AFSP’s Facebook page at 4:30pm EST on Saturday, November 17th. This will be an online interactive discussion with a screening of our 2018 Documentary, A Daughter’s Journey.</strong></p>
 				<p>Survivor Day is the one day a year when people affected by suicide loss gather around the world at events in their local communities to find comfort and gain understanding as they share stories of healing and hope. <a href="<?php get_home_url(); ?>/survivorday" target="_blank">Discover more about Survivor Day.</a></p>
 				<?php
 				$reg_begins = strtotime( '07/01/17' );
