2b450d609cbfcd71a97d8c48de2366ac70e34439
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-04-17 09:39:19 -0400

    added modal to toolkit v2

--- wp-content/themes/afsp/page-templates/campaign-toolkit.php
+++ wp-content/themes/afsp/page-templates/campaign-toolkit.php
@@ -304,7 +304,8 @@ if(have_posts()) : while(have_posts()) : the_post();
     ?>
       <div class="grid-item">
         <img class="grid-image" src="<?php echo $child_image_url; ?>" />
-    <?php if ( get_sub_field( 't_has_modal' ) === 'Yes' ) : ?>
+    <?php if ( get_sub_field( 't_has_modal' ) === 'Yes' ) : 
+      echo 'has modal'; ?>
         <div class="grid-overlay">
           <div class="grid-item-description"><?php the_sub_field( 't_grid_child_description' ); ?></div>
           <a class="grid-button" href="<?php the_sub_field( 't_link' ) ?>" target="_blank"><?php the_sub_field( 't_grid_child_cta' ) ?></a>
@@ -313,7 +314,8 @@ if(have_posts()) : while(have_posts()) : the_post();
           <div><svg class="modal__close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 99.082 100" enable-background="new 0 0 99.082 100" xml:space="preserve"><g><g><path d="M49.54,0C22.198-0.019,0.027,22.375,0,49.973C0.027,77.618,22.198,100.01,49.54,100    c27.352,0.01,49.521-22.382,49.542-50.027C99.062,22.375,76.892-0.019,49.54,0z M49.54,88.477    c-21.037,0-38.095-17.225-38.077-38.504C11.442,28.741,28.503,11.51,49.54,11.521c21.047-0.01,38.107,17.221,38.131,38.452    C87.647,71.252,70.587,88.477,49.54,88.477z"></path></g><polygon points="71.57,37.466 62.073,27.97 49.54,40.504 37.006,27.97 27.509,37.466 40.047,50.007 27.509,62.534 37.006,72.03    49.54,59.496 62.073,72.03 71.57,62.534 59.036,50.007  "></polygon></g></svg></div>
           <div class="grid-modal-content"><?php the_sub_field( 't_modal_content' ); ?></div>
         </div>
-    <?php elseif ( get_sub_field( 't_has_modal' ) === 'No' ) : ?>
+    <?php elseif ( get_sub_field( 't_has_modal' ) === 'No' ) : 
+      echo 'no modal'; ?>
         <div class="grid-overlay">
           <div class="grid-item-description"><?php the_sub_field( 't_grid_child_description' ); ?></div>
           <a class="grid-button" href="<?php the_sub_field( 't_link' ) ?>" target="_blank"><?php the_sub_field( 't_grid_child_cta' ) ?></a>
