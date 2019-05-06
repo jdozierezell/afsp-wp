e989cac1b57168e0d73616a4aba52cc2c122b5c9
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-05-06 15:27:44 -0400

    recommiting

--- wp-content/themes/cl2/assets/scripts/business-card.js
+++ wp-content/themes/cl2/assets/scripts/business-card.js
@@ -1,7 +1,7 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-/******/
+/******/  // Just a comment
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/
