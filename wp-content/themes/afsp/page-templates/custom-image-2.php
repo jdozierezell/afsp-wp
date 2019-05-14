01367df349f194b3ed92a5241bc0d1ce651e78bd
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-05-14 11:53:49 -0400

    finally editing the right file!!

--- wp-content/themes/afsp/page-templates/custom-image-2.php
+++ wp-content/themes/afsp/page-templates/custom-image-2.php
@@ -225,8 +225,7 @@
 </script>
 
 
-				<?php echo get_theme_file_path('/template-parts/canvas/image-upload-2.php');
-                require get_theme_file_path('/template-parts/canvas/image-upload-2.php');
+				<?php require locate_template('/template-parts/canvas/image-upload-2.php');
 				include(locate_template('template-parts/canvas/image-download.php')); ?>
 
 
