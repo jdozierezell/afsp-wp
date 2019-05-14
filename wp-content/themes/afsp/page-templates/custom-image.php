33a84591e62ca85fe491f1ce59e494e0e4aa7967
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-05-14 10:57:14 -0400

    now using image-upload-2 v15

--- wp-content/themes/afsp/page-templates/custom-image.php
+++ wp-content/themes/afsp/page-templates/custom-image.php
@@ -225,7 +225,7 @@
 </script>
 
 
-				<?php require(locate_template('template-parts/canvas/image-upload.php'));
+				<?php require(locate_template('template-parts/canvas/image-upload-2.php'));
 				require(locate_template('template-parts/canvas/image-download.php')); ?>
 
 
