98651c2d5f3e91e4052efecd9139946e44353bcc
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-05-15 10:13:23 -0400

    added load image api v7

--- wp-content/themes/afsp/template-parts/canvas/image-upload-2.php
+++ wp-content/themes/afsp/template-parts/canvas/image-upload-2.php
@@ -10,59 +10,38 @@
 
 
 	function handleImage(e){
-	    var loadingImage = loadImage(
-	        e.target.files[0],
-            function(img, data) {
-	            document.getElementById('measure').appendChild(img)
-                var wrh = img.width / img.height
-                var newWidth = canvas.width;
-                var newHeight = newWidth / wrh;
-                if (newHeight < canvas.height) {
-                    newHeight = canvas.height;
-                    newWidth = newHeight * wrh;
-                }
-                console.log('Exif data: ', data.exif)
-                ctx.drawImage(img,(canvas.width-newWidth)/2,(canvas.height-newHeight)/2,newWidth,newHeight);
-            },
-            {orientation: 1}
-        )
-	    // var reader = new FileReader();
-	    // reader.onload = function(event){
-        // var img = new Image();
-        // img.onload = function(){
-        // 	var ct = document.getElementById('measure');
-        // 	ct.appendChild(img);
-        // 	var wrh = img.width / img.height
-        //     var newWidth = canvas.width;
-        //     var newHeight = newWidth / wrh;
-        //     if (newHeight < canvas.height) {
-        //         newHeight = canvas.height;
-        //         newWidth = newHeight * wrh;
-        //     }
-
-
-            // img.prop('src', reader.result)
-            // img.src = img.prop('src')
-            // EXIF.getData(img, function() {
-            //     console.log(EXIF.pretty(img))
-            // })
-
-            // console.log(orientation)
-            // switch (orientation) {
-            //     case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
-            //     case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
-            //     case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
-            //     case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
-            //     case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
-            //     case 7: ctx.transform(0, -1, -1, 0, height, width); break;
-            //     case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
-            //     default: break;
-            // }
-        //     ctx.drawImage(img,(canvas.width-newWidth)/2,(canvas.height-newHeight)/2,newWidth,newHeight);
-        // }
-        // img.src = event.target.result;
-	    // }
-	    // reader.readAsDataURL(e.target.files[0]);
+	    var reader = new FileReader();
+	    reader.onload = function(event){
+        var img = new Image();
+        img.onload = function(){
+        	var ct = document.getElementById('measure');
+        	ct.appendChild(img);
+        	var wrh = img.width / img.height
+            var newWidth = canvas.width;
+            var newHeight = newWidth / wrh;
+            if (newHeight < canvas.height) {
+                newHeight = canvas.height;
+                newWidth = newHeight * wrh;
+            }
+            var orientation = EXIF.getData(img, function() {
+                return EXIF.getTag(this, 'Orientation')
+            })
+            console.log(orientation)
+            switch (orientation) {
+                case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+                case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+                case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+                case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+                case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+                case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+                case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+                default: break;
+            }
+            ctx.drawImage(img,(canvas.width-newWidth)/2,(canvas.height-newHeight)/2,newWidth,newHeight);
+        }
+        img.src = event.target.result;
+	    }
+	    reader.readAsDataURL(e.target.files[0]);
 	}
 
 </script>
