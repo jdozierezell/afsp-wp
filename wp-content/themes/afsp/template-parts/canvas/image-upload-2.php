af9554114fa509365c5205cb7d54c94762b517de
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-05-15 16:14:48 -0400

    added load image api v19

--- wp-content/themes/afsp/template-parts/canvas/image-upload-2.php
+++ wp-content/themes/afsp/template-parts/canvas/image-upload-2.php
@@ -14,7 +14,16 @@
         loadImage(
             e.target.files[0],
             function (img) {
-                document.getElementById('measure').appendChild(img)
+                var ct = document.getElementById('measure');
+                ct.appendChild(img);
+                var wrh = img.width / img.height;
+                var newWidth = canvas.width;
+                var newHeight = newWidth / wrh;
+                if (newHeight < canvas.height) {
+                    newHeight = canvas.height;
+                    newWidth = newHeight * wrh;
+                }
+                ctx.drawImage(img,(canvas.width-newWidth)/2,(canvas.height-newHeight)/2,newWidth,newHeight);
             },
             {orientation: 1}
         )
