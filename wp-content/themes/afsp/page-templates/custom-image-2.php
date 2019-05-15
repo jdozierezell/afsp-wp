f7bb2b758313e3d144a8d40ec1490f7dd84f2493
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-05-15 10:51:03 -0400

    added load image api v10

--- wp-content/themes/afsp/page-templates/custom-image-2.php
+++ wp-content/themes/afsp/page-templates/custom-image-2.php
@@ -55,8 +55,7 @@
   }(document, 'script', 'facebook-jssdk'));
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/exif-js/2.1.0/exif.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/blueimp-load-image/2.12.2/load-image.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/blueimp-load-image/2.12.2/load-image-orientation.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/load-image.all.min.js"></script>
 <?php
     $backgroundImage = get_field( 'ci_background_image' );
     $backgroundAFSPUrl = $backgroundImage['url'];
--- wp-content/themes/afsp/template-parts/canvas/image-upload-2.php
+++ wp-content/themes/afsp/template-parts/canvas/image-upload-2.php
@@ -1,46 +1,42 @@
 <?php // Source: http://jsfiddle.net/influenztial/qy7h5/
-// Image scaling trick courtesy of http://stackoverflow.com/questions/28047792/html-canvas-scaling-image-to-fit-without-stretching?>
+    // Image scaling trick courtesy of http://stackoverflow.com/questions/28047792/html-canvas-scaling-image-to-fit-without-stretching?>
 <div id="measure" style="position: absolute; left: -10000px; top: -100000px;"></div>
 <script type="text/javascript">
 
-	var imageLoader = document.getElementById('<?php echo $loaderID; ?>');
-	    imageLoader.addEventListener('change', handleImage, false);
-	var canvas = document.getElementById('<?php echo $canvasID; ?>');
-	var ctx = canvas.getContext('2d');
+    var imageLoader = document.getElementById('<?php echo $loaderID; ?>');
+    imageLoader.addEventListener('change', handleImage, false);
+    var canvas = document.getElementById('<?php echo $canvasID; ?>');
+    var ctx = canvas.getContext('2d');
 
 
-	function handleImage(e){
-	    var reader = new FileReader();
-	    reader.onload = function(event){
-        var img = new Image();
-        img.onload = function(){
-        	var ct = document.getElementById('measure');
-        	ct.appendChild(img);
-        	var wrh = img.width / img.height
-            var newWidth = canvas.width;
-            var newHeight = newWidth / wrh;
-            if (newHeight < canvas.height) {
-                newHeight = canvas.height;
-                newWidth = newHeight * wrh;
+    function handleImage(e){
+
+        loadImage(
+            e.target.files[0],
+            function (img, data) {
+                console.log(data.exif)
             }
-            EXIF.getData(img, function() {
-                console.log(EXIF.getTag(this, 'Orientation'))
-            })
-            switch (orientation) {
-                case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
-                case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
-                case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
-                case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
-                case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
-                case 7: ctx.transform(0, -1, -1, 0, height, width); break;
-                case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
-                default: break;
-            }
-            ctx.drawImage(img,(canvas.width-newWidth)/2,(canvas.height-newHeight)/2,newWidth,newHeight);
+        )
+
+
+        // var reader = new FileReader();
+        // reader.onload = function(event){
+        //     var img = new Image();
+        //     img.onload = function(){
+        //         var ct = document.getElementById('measure');
+        //         ct.appendChild(img);
+        //         var wrh = img.width / img.height;
+        //         var newWidth = canvas.width;
+        //         var newHeight = newWidth / wrh;
+        //         if (newHeight < canvas.height) {
+        //             newHeight = canvas.height;
+        //             newWidth = newHeight * wrh;
+        //         }
+        //         ctx.drawImage(img,(canvas.width-newWidth)/2,(canvas.height-newHeight)/2,newWidth,newHeight);
+        //     }
+        //     img.src = event.target.result;
         }
-        img.src = event.target.result;
-	    }
-	    reader.readAsDataURL(e.target.files[0]);
-	}
+        reader.readAsDataURL(e.target.files[0]);
+    }
 
 </script>
