73c6df333803b114b782466d2e86f674b07b823b
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-05-31 16:24:23 -0400

    updating footable data on find an isosld event v9

--- wp-content/themes/afsp/page-templates/find-a-survivor-day-2.php
+++ wp-content/themes/afsp/page-templates/find-a-survivor-day-2.php
@@ -30,23 +30,32 @@ if ( have_posts() ) :
             <!-- Table Markup -->
             <table id="isosld" class="tablepress">
                 <thead>
-                    <tr>
-                        <th>Event Name</th>
-                        <th>City</th>
-                        <th>State/Province</th>
-                        <th>Postal Code</th>
-                        <th>Country</th>
-                    </tr>
+                <tr>
+                    <th>Event Name</th>
+                    <th>City</th>
+                    <th>State/Province</th>
+                    <th>Postal Code</th>
+                    <th>Country</th>
+                </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Montgomery, Alabama</td>
-                        <td>Montgomery</td>
-                        <td>Alabama</td>
-                        <td>36109</td>
-                        <td>United States of America</td>
-                    </tr>
+                <tr>
+                    <th>Event Name</th>
+                    <th>City</th>
+                    <th>State/Province</th>
+                    <th>Postal Code</th>
+                    <th>Country</th>
+                </tr>
+                <tr>
+                    <td>Montgomery, Alabama</td>
+                    <td>Montgomery</td>
+                    <td>Alabama</td>
+                    <td>36109</td>
+                    <td>United States of America</td>
+                </tr>
                 </tbody>
+                <tfoot>
+                </tfoot>
             </table>
         </section>
 		<section class="container">
@@ -60,26 +69,30 @@ if ( have_posts() ) :
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css"/>
         <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
         <script>
-            jQuery(document).ready(function() {
-                jQuery('#isosld').DataTable({
-                    initComplete: function () {
-                        this.api().columns().every(function() {
-                            var column = this
-                            var select = jQuery('<select><option value=""></option></select>')
-                                .appendTo(jQuery(column.footer()).empty())
-                                .on('change', function() {
-                                    var val = jQuery.fn.dataTable.util.excapeRegex(
-                                        jQuery(this).val()
-                                    )
-                                    column.search(val ? '^' + val + '$' : '', true, false).draw()
-                                })
-                            column.data().unique().sort().each(function(d, j) {
-                                select.append('<option value="' + d + '">' + d + '<option>')
-                            })
-                        })
-                    }
-                                      })
-            })
+            $(document).ready(function() {
+                $('#isosld').DataTable( {
+                                            initComplete: function () {
+                                                this.api().columns().every( function () {
+                                                    var column = this;
+                                                    var select = $('<select><option value=""></option></select>')
+                                                        .appendTo( $(column.header()).empty() )
+                                                        .on( 'change', function () {
+                                                            var val = $.fn.dataTable.util.escapeRegex(
+                                                                $(this).val()
+                                                            );
+
+                                                            column
+                                                                .search( val ? '^'+val+'$' : '', true, false )
+                                                                .draw();
+                                                        } );
+
+                                                    column.data().unique().sort().each( function ( d, j ) {
+                                                        select.append( '<option value="'+d+'">'+d+'</option>' )
+                                                    } );
+                                                } );
+                                            }
+                                        } );
+            } );
         </script>
 
 	<?php
