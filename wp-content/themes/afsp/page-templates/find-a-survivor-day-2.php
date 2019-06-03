3ca80aab76a5e9a79cff41b791589b0e56240bac
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-06-03 14:57:33 -0400

    lighting up the isosld template v5

--- wp-content/themes/afsp/page-templates/find-a-survivor-day-2.php
+++ wp-content/themes/afsp/page-templates/find-a-survivor-day-2.php
@@ -97,6 +97,7 @@ if ( have_posts() ) :
 
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css"/>
         <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
+        <script type="text/javascript" src="https://cdn.datatables.net/plug-ins/1.10.19/api/fnFilterClear.js"></script>
         <script>
             jQuery(document).ready(function() {
                 jQuery('#isosld').DataTable( {
