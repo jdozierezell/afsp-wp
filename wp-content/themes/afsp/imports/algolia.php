bdffdb19cbd8e9e638b1592452875508d1a47443
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-08-03 19:37:17 -0400

    adding algolia for donor drive v5

--- wp-content/themes/afsp/imports/algolia.php
+++ wp-content/themes/afsp/imports/algolia.php
@@ -1,9 +1,9 @@
 <?php
 // composer autoload
-require __DIR__ . '/vendor/autoload.php';
-echo 'howdy';
+// require __DIR__ . '/vendor/autoload.php';
+
 // if you are not using composer
-// require_once 'path/to/algolia/folder/autoload.php';
+require_once 'algoliasearch-client-php-master/autoload.php';
 
 $client = Algolia\AlgoliaSearch\SearchClient::create(
   'BONWJFMMRS',
@@ -13,7 +13,7 @@ $client = Algolia\AlgoliaSearch\SearchClient::create(
 // $index = $client->initIndex('Donor Drive');
 $batch = json_decode(file_get_contents('merged.json'), true);
 echo '<pre>'.$batch.'</pre>';
-// $index->saveObjects($batch, ['autoGenerateObjectIDIfNotExist' => true]);
+$index->saveObjects($batch, ['autoGenerateObjectIDIfNotExist' => true]);
 
 // $index->setSettings(
 //   [
