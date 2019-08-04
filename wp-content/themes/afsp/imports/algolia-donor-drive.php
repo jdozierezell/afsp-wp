c167fbdc0417abd882fa99d64608bcaa85f7389f
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-08-03 20:34:48 -0400

    adding algolia for donor drive v20

--- wp-content/themes/afsp/imports/algolia-donor-drive.php
+++ wp-content/themes/afsp/imports/algolia-donor-drive.php
@@ -2,27 +2,28 @@
 
 	$filename = get_template_directory() . '/imports/donor-drive.xml';
 
-  libxml_use_internal_errors(true);
-  $xml = simplexml_load_file($filename);
-var_dump($xml);
-  $json = json_encode($xml);
-  $array = json_decode($json, true);
+//   libxml_use_internal_errors(true);
+//   $xml = simplexml_load_file($filename);
+//   $json = json_encode($xml);
+//   $array = json_decode($json, true);
 
-  if(!$array) : // make sure donor drive isn't being f*cking stupid
-    return;
-  endif;
+//   if(!$array) : // make sure donor drive isn't being f*cking stupid
+//     return;
+//   endif;
 
-  $json_file = get_template_directory() . '/imports/algolia-merged.json';
+//   $json_file = get_template_directory() . '/imports/algolia-merged.json';
 
-  if(!file_exists($filename)) {
-    echo 'File not found.';
-  }
+//   if(!file_exists($filename)) {
+//     echo 'File not found.';
+//   }
 
-  $fp = fopen($json_file, 'w');
+//   $fp = fopen($json_file, 'w');
 
-  if(!$fp) {
-      echo 'File open failed.';
-  }
+//   if(!$fp) {
+//       echo 'File open failed.';
+//   }
 
-  fwrite($fp, json_encode($array));
-  fclose($fp);
+//   fwrite($fp, json_encode($array));
+//   fclose($fp);
+
+var_dump($filename);
